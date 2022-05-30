7075f3a7db99eb143684a3e0b757bed88b5772fb
ranjayJava <89581687+ranjayJava@users.noreply.github.com>
2022-05-30 10:38:15 +0530

    added basic structure

---
+++ src/main/java/com/org/exceptions/RecordAlreadyPresentException.java
@@ -0,0 +1,12 @@
+package com.org.exceptions;
+
+public class RecordAlreadyPresentException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RecordAlreadyPresentException(String s) {
+		super(s);
+	}
+}
